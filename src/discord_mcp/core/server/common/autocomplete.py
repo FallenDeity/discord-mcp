fe29e443e3c1c2cd2941df6453e2bd8469cced8b
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2025-08-02 23:06:15 +0200

    add autocomplete docstrings

--- src/discord_mcp/core/server/common/autocomplete.py
+++ src/discord_mcp/core/server/common/autocomplete.py
@@ -36,7 +36,27 @@ class AutoCompletable:
     autocomplete_handler: AutocompleteHandler
 
     def autocomplete(self, argument_name: str) -> t.Callable[[AutocompleteCallback], AutocompleteCallback]:
-        """Provides completions for prompts and resource templates"""
+        """Provides completions for prompts and resource templates.
+
+        The decorated function must take exactly 3 arguments:
+            - a ``reference`` which has the type `DiscordMCPPrompt` | `DiscordMCPResourceTemplate`
+            - an ``argument`` which has the type of the autocompleted argument
+            - a ``context`` which has the type of `dict[str, Any]`
+
+        Note:
+            The return type can be any, it is wrapped internally. If you want to override this behaviour
+            you just need to return a `mcp.types.Completion` object. All the other objects are automatically
+            wrapped.
+
+        Note:
+            The ``argument`` parameter contains the user input casted to python a python object. The same applies
+            for ``context``.
+            It is important that the autocompleted argument in the parent function has the correct type hint
+            for the value cast to work correctly.
+
+        Args:
+            argument_name: The name of the argument to autocomplete.
+        """
         return self.autocomplete_handler.autocomplete(argument_name)
 
 
@@ -80,7 +100,7 @@ class AutocompleteHandler:
         except KeyError as e:
             # normally should never happen if you don't play at removing
             # handler from the managers at runtime
-            raise RuntimeError("An autocomplete for an unregistered prompt or template has been called!") from e
+            raise RuntimeError("An autocomplete for an unregistered prompt or template has been called.") from e
 
         promoted_argument_value = convert_string_arguments(promoted.fn, {argument.name: argument.value})[argument.name]
         if context and context.arguments:
